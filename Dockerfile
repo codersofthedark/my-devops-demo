f16e3251b55fd43c0eb6fe21283872bc8e9023b8
Utsav Sabharwal <codersofthedark@gmail.com>
2018-11-28 17:29:17 +0530

    First Commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM 2.7.15-jessie
+RUN pip install -r requirements.txt
+EXPOSE 5000
+WORKDIR /
+COPY code.py code.py
+ENTRYPOINT FLASK_APP=hello.py flask run
---
+++ code.py
@@ -0,0 +1,17 @@
+#Benchmarking SQS
+from flask import Flask, request
+import boto3
+import logging
+logging.basicConfig(handlers=[logging.StreamHandler()],format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    logging.debug(request.path)
+    logging.debug(request.method)
+    logging.info(request.headers)
+    return "Request Completed"
+
+
+
